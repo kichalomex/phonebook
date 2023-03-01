d18fedb9abf421ff68d5b2145c2e490a76f0c18b
kichalomex <kichalo23con@hotmail.com>
2023-02-28 20:14:48 -0600

    Paso 2
    paso 2.7

--- src/components/Apps.js
+++ src/components/Apps.js
@@ -5,32 +5,38 @@ import { Persons } from "./Persons"
 const App = (props) => {
     const [ persons, setPersons ] = useState (props.persons)
     const [ newName, setNewName ] = useState('')
-    
+
     const handleSubmit = (event) =>{
-      event.preventDefault()
-  
-      const newObject = { 'name': newName}
-      setPersons(persons.concat(newObject))
-      setNewName('')
+        event.preventDefault()
+        
+        const persons_names = persons.map(person => person.name)
+        if (persons_names.includes(newName)) {
+            alert(`${newName} is already added to phoneBook.`)
+        }
+        else {
+            const newObject = { 'name': newName}
+            setPersons(persons.concat(newObject))
+            setNewName('')
+        }
     }
     return (
-      <div>
-        <Header name={'Phonebook'}/>
-        <form onSubmit={handleSubmit}>
-          <div>
-            name: <input values={newName} onChange=
-            {handleChange(setNewName)}/>
-          </div>
-          <div>
-            <button type='submit'>add</button>
-          </div>
-        </form>
         <div>
-          <Header name={'Numbers'}/>
-          <Persons persons={persons}/>
+            <Header name={'Phonebook'}/>
+            <form onSubmit={handleSubmit}>
+            <div>
+                name: <input values={newName} onChange=
+                {handleChange(setNewName)}/>
+            </div>
+            <div>
+                <button type='submit'>add</button>
+            </div>
+            </form>
+            <div>
+            <Header name={'Numbers'}/>
+            <Persons persons={persons}/>
+            </div>
         </div>
-      </div>
-      
+        
     )
   }
   const Header = (props) => {
